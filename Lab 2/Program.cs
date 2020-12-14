9a0cfc32986d7bab002a8fa0a6ff5a726b81530a
ilyadery <ilyadrbn@gmail.com>
2020-12-14 12:35:27 +0200

    Добавьте файлы проекта.

---
+++ Lab 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, father, family, number;
+
+            Console.Write("Введите имя: ");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("Имя: " + name);
+
+            Console.Write("Введите отчество: ");
+
+            father = Console.ReadLine();
+
+            Console.WriteLine("Отчество: " + father);
+
+            Console.Write("Введите фамилию: ");
+
+            family = Console.ReadLine();
+
+            Console.WriteLine("Фамилия: " + family);
+
+            Console.Write("Введите номер: ");
+
+            number = Console.ReadLine();
+
+            Console.WriteLine("Номер: " + number);
+
+            Console.WriteLine("Здравствуйте " + name + " " + father + " " + family + "." + " Вы " + number + " по списку");
+        }
+    }
+}
